69f9f55a04b75b4e9e560e764d622bf3d50a31ba
AnshikaaTomar <anshika.singh5417@gmail.com>
2026-03-24 16:30:18 +0530

    Poppler path configured efficiently

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import pytesseract
 
 # path definitions
@@ -6,7 +7,7 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 input_dir = os.path.join(base_dir, "inputs")
 output_dir = os.path.join(base_dir, "outputs")  
 
-poppler_path = "C:\\poppler-25.12.0\\Library\\bin"
+poppler_path = POPPLER_PATH = r"C:\\poppler-25.12.0\\Library\\bin" if platform.system() == "Windows" else None
 
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
